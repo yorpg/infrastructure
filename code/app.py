a8483cbd3215a10977ad50ece739be6ce1b19683
Sim@Home <simon@s-m-y.com>
2018-09-06 20:35:56 +0300

    show incoming parameters

--- code/app.py
+++ code/app.py
@@ -82,6 +82,6 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps(
-            {"message": "hello world", "location": ip.text.replace("\n", "")}
+            {"message": "hi! world", "location": event}
         ),
     }
--- scripts/build.bat
+++ scripts/build.bat
@@ -2,5 +2,5 @@ set code_path=code
 pip install  -qq -r requirements.txt -t build/%code_path%/
 cp %code_path%/*.py build/%code_path%/
 
-sam package --template-file template.yaml --output-template-file build\packaged.yaml --s3-bucket yorpg-develop
-aws cloudformation deploy --template-file build\packaged.yaml --stack-name yorpg-develop --capabilities CAPABILITY_IAM
+rem sam package --template-file template.yaml --output-template-file build\packaged.yaml --s3-bucket yorpg-develop
+rem aws cloudformation deploy --template-file build\packaged.yaml --stack-name yorpg-develop --capabilities CAPABILITY_IAM
